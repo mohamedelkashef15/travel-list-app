fa61877cda981fe32ee82e6d4e88dcf06fcec417
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-07-30 03:46:28 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+// ];
+
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
